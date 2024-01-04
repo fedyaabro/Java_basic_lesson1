8a4f9856c01d558aac21afc9c3ca5c53e97ca6f7
Fedor Abrosimov <fedor.abrosimov94@gmail.com>
2024-01-04 20:18:10 +0300

    refactoring

--- src/main/java/guru/hw3/DataTypes.java
+++ src/main/java/guru/hw3/DataTypes.java
@@ -7,23 +7,31 @@ import java.util.List;
 /**void Означает, что после выполнения кода anotherExample не будет результата**/
 
 public class DataTypes{
-  void anotherExample(){
-    //базовые типы данных
-     //целочисленные типы
+  
+  void simpleTypesAndWrappers() {
+    //  примитивные типы данных + обертки
+    //целочисленные типы
     byte aByte = 0; // -127 +127 8bit
+    Byte byteWrapper = 0; // -127 +127 8bit
     short aShort = 0;// -32768 +32767 16bit
+    Short shortWrapper = 0;
     int aInt = 0; // (-2^32) ...(-2^32-1) 32b
+    Integer intWrapper = 0;
     long aLong = 0; //
-     //числа с плавающей точкой
+    //числа с плавающей точкой
     float aFloat = 0.0F;
+    Float floatWrapper = 0.0F;
     double aDouble = 0.0;
-     //Символьный
+    Double doubleWrapper = 0.0;
+    //Символьный
     char aChar = 'a';
-     //Логический
+    Character charWrapper = 'a';
+    //Логический
     boolean aBoolean = true;
-    
-    //Присвоения
-    String name = "Вьше";
+    Boolean booleanWripper = true;
+  }
+  
+  void mathOperators() {
     //Арифметические + - /  * % ++ --
     System.out.println(4.0 + 3);
     
@@ -37,15 +45,19 @@ public class DataTypes{
     System.out.println("b / a = " + (b / a));
     System.out.println("b % a = " + (b % a));
     System.out.println("c % a = " + (c % a));
-    System.out.println("a++   = " +  (++a));
-    System.out.println("b--   = " +  (--a));
+    System.out.println("a++   = " + (++a));
+    System.out.println("b--   = " + (--a));
     // Проверьте разницу в d++ и ++d
-    System.out.println("d++   = " +  (d++));
-    System.out.println("++d   = " +  (++d));
     
+    /**Постфиксная и префиксная запись инкрементов и дикрементов - очень интересно, но понятно не сразу**/
+    System.out.println("d++   = " + (d++));
+    System.out.println("++d   = " + (++d));
+  }
+  
+  void compareOperators() {
     //Сравнения > < >= <= != ==
     /**Нельза стравнивать == данные объектого типа, например String - нужно использовать equals**/
-    System.out.println(3>=2);
+    System.out.println(3 >= 2);
     //Логические операторы &, &&, |, ||, !
     /** && не вычесляет остаток выражения, если результат уже известен**/
     String myName = "Fedor";
@@ -54,24 +66,59 @@ public class DataTypes{
     System.out.println(myName.equals("Fedor") && myAge == 29);
     System.out.println(myName.equals("Fedor") || myAge == 27);
     System.out.println(!(myName.equals("Fedor") || myAge == 27));
-    
+  }
+  
+  void appropriationOperators(){
+    //Присвоения
+    String name = "Вьше";
+  }
+  
+  void conditionalOperators(){
     //Условный оператор if/else
+    Character sex = 'm';
+    String childName;
+    
+    int a = 10;
+    int b = 20;
     
     if(a >= b){
       System.out.println("a > b");
-    }
-    else{
+    } else{
       System.out.println("b > a");
     }
     
+    if(sex == 'm'){
+      childName = "Valentin";
+    }else {
+      childName = "Valentina";
+    }
+    System.out.println(childName);
     
+    //тернарный оператор
+    childName = sex == 'm'
+      ? "Valentin"
+      : "Valentina";
+    System.out.println(childName);
     
-    //Строка ( и бесконечность объектных/ссылочных типов данных)
-    String toBePrint = "HelloWorld";
-    //Список
-    List<String> teachers = List.of("1","2");
+    //switch/case
+    int num = 8;
+    switch(num){
+      
+      case 1:
+        System.out.println("число равно 1");
+        break;
+      case 8:
+        System.out.println("число равно 8");
+        num++;
+        break;
+      case 9:
+        System.out.println("число равно 9");
+        break;
+      default:
+        System.out.println("число не равно 1, 8, 9");
+    }
     
   }
-
-    }
+  }
+  
     
